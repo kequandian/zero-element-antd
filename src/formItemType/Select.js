078e6f2e68a631573864132e3573ea6ca6068c6b
LLH <youyeke@163.com>
2019-07-09 10:23:45 +0800

    Select 处理 change

--- src/formItemType/Select.js
+++ src/formItemType/Select.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { Select } from 'antd';
 
 export default function SelectWrapped(props) {
-  const { options = [] } = props;
-  return <Select>
+  const { options = [], onChange } = props;
+
+  function hanldeChange(value) {
+    onChange(value);
+  }
+
+  return <Select onChange={hanldeChange}>
     {options.map((option, i) =>
       <Select.Option value={option.value} key={i}>
         {option.label}
