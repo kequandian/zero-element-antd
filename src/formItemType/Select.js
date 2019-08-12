31d2c5d23a87271da4e3ba7fb705bb7c9c35f1e8
LLH <youyeke@163.com>
2019-08-12 15:49:07 +0800

    fixed Select value 不能重置的问题

--- src/formItemType/Select.js
+++ src/formItemType/Select.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Select } from 'antd';
 
 export default function SelectWrapped(props) {
-  const { options = [], onChange } = props;
+  const { value, options = [], onChange } = props;
 
   function hanldeChange(value) {
     onChange(value);
   }
 
-  return <Select onChange={hanldeChange}>
+  return <Select onChange={hanldeChange} value={value}>
     {options.map((option, i) =>
       <Select.Option value={option.value} key={i}>
         {option.label}
