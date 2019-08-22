a7fe25cd9059a42186d50bb6de9d29b720e73938
LLH <youyeke@163.com>
2019-08-22 11:31:32 +0800

    使用 css 来替换原本的 a 标签

--- src/container/List/components/ListOperation/type/Outside.js
+++ src/container/List/components/ListOperation/type/Outside.js
@@ -3,9 +3,7 @@ import React from 'react';
 export default (item, i, { index, record }, onAction) => {
   return (
     <span key={i} onClick={onAction.bind(null, item.action, item.options)} className="ZEleA-table-action-Outside-normal">
-      <a href="javascript:;">
-        {item.title}
-      </a>
+      {item.title}
     </span>
   )
 }
--- src/container/List/components/ListOperation/type/Switch.js
+++ src/container/List/components/ListOperation/type/Switch.js
@@ -5,10 +5,8 @@ export default (item, i, { index, record }, onAction) => {
   const { field = 'enabled' } = item.options;
   const enabled = Boolean(record[field]);
   return (
-    <span key={i}>
-      <a href="javascript:;" onClick={onAction.bind(null, item.action, item.options)}>
-        <Switch size="small" checked={enabled} />
-      </a>
+    <span key={i} onClick={onAction.bind(null, item.action, item.options)}>
+      <Switch size="small" checked={enabled} />
     </span>
   )
 }
