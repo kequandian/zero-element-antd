9e2ada0ceac5962ff6dfc393110b8dffb5e16eb8
LLH <youyeke@163.com>
2019-08-23 11:14:21 +0800

    处理下载文件的 listAction request

--- src/listAction/onRequest.js
+++ src/listAction/onRequest.js
@@ -1,4 +1,4 @@
-import { query, post, update, remove } from 'zero-element/lib/utils/request';
+import { query, post, update, remove, download } from 'zero-element/lib/utils/request';
 import { message as msg } from 'antd';
 
 const methodMap = {
@@ -6,13 +6,27 @@ const methodMap = {
   'post': post,
   'put': update,
   'delete': remove,
+  'download': download,
 };
 
 export default function onRequest({ options, record }) {
-  const { API, method = 'get', message = '操作成功' } = options;
+  const { API, method = 'get', message = '操作成功', data = {} } = options;
   const match = methodMap[method];
 
-  return match(API)
+  if (method === 'download') {
+    return download(API, {
+      method: options.downloadMethod,
+      fileName: options.fileName,
+    })
+      .then(_ => {
+        if (message) {
+          msg.success(message);
+        }
+      })
+      .catch(_ => msg.error(JSON.stringify(_)));
+  }
+
+  return match(API, data)
     .then(_ => {
       if (message) {
         msg.success(message);
