7ea4a25756d807ec65d3a584b5eb9af7bf36463f
LLH <youyeke@163.com>
2019-10-12 17:07:42 +0800

    valueType tag 也支持 map 功能

--- src/valueType/tag/index.js
+++ src/valueType/tag/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Tag } from 'antd';
 import config from './tag.config';
+import defaultMap from '../status/status.config';
 
 export default function valueTypeTag(props) {
   const { options = {}, data: { text = '' } } = props;
-  const { colorMap = {} } = options;
-  return <Tag color={colorMap[text] || config[text] || '#108ee9'}>{text}</Tag>
+  const { colorMap = {}, map = {} } = options;
+  return <Tag color={colorMap[text] || config[text] || '#108ee9'}>
+    {map[text] || defaultMap[text] || text}
+  </Tag>
 }
