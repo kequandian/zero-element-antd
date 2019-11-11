f6dbb6a686b72aaea4a8031745f8c9c287323dda
LLH <youyeke@163.com>
2019-11-11 18:18:27 +0800

    fixed RichText 数据初始化的问题

--- src/formItemType/RichText.js
+++ src/formItemType/RichText.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BraftEditor from 'braft-editor';
 import { useDidMount } from 'zero-element/lib/utils/hooks/lifeCycle';
 
 import 'braft-editor/dist/index.css';
 
 export default (props) => {
-  const { name, handle, props: p, ...rest } = props;
+  const { name, value, handle, onChange, props: p, ...rest } = props;
+  const [editorState, setEditorState] = useState();
 
   useDidMount(_ => {
     handle.onFormatValue(name, 'html');
   });
 
+  useEffect(_ => {
+    setEditorState(BraftEditor.createEditorState(value));
+  }, [value]);
+
+  function handleChange(editorState) {
+    setEditorState(editorState);
+    onChange(editorState);
+  }
+
   return <BraftEditor
     name={name}
     {...rest}
     {...p}
+    value={editorState}
+    onChange={handleChange}
     placeholder="请输入内容"
   />
 }
