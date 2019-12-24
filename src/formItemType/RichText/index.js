a70b5b55e8cc006b0f0e4e8a2da0fc23beef4548
LLH <youyeke@163.com>
2019-12-24 11:11:24 +0800

    富文本编辑，启用 api 上传功能

--- src/formItemType/RichText/index.js
+++ src/formItemType/RichText/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import BraftEditor from 'braft-editor';
 import { useDidMount } from 'zero-element/lib/utils/hooks/lifeCycle';
+import uploadFile from './uploadFile';
 
 import 'braft-editor/dist/index.css';
 
 export default function RichText(props) {
-  const { name, value, handle, onChange, props: p, ...rest } = props;
+  const { name, value, handle, onChange, options, props: p, ...rest } = props;
+  const { API = '/api/fs/uploadfile' } = options;
   const [editorState, setEditorState] = useState();
 
   useDidMount(_ => {
@@ -18,12 +20,17 @@ export default function RichText(props) {
     }
   }, [value]);
 
+  const media = {
+    uploadFn: uploadFile.bind(null, API),
+  };
+
   if (editorState) {
     return <BraftEditor
       name={name}
       {...rest}
       {...p}
       defaultValue={editorState}
+      media={media}
       onBlur={onChange}
       placeholder="请输入内容"
     />
@@ -32,6 +39,7 @@ export default function RichText(props) {
     name={name}
     {...rest}
     {...p}
+    media={media}
     onBlur={onChange}
     placeholder="请输入内容"
   />
