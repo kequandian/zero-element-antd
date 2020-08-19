61cee28b33ae64e6188b8161e4169563848a7225
LLH <youyeke@163.com>
2020-08-19 10:09:23 +0800

    formType password 增加 notUpdateSubmitNull 选项

--- src/formItemType/Password.js
+++ src/formItemType/Password.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Input } from 'antd';
+import { useWillMount } from 'zero-element/lib/utils/hooks/lifeCycle';
 
 const { Password } = Input;
 
-export default ({ props, ...rest }) => {
-  return <Password {...rest} {...props} />;
+export default ({ name, props, value, options, onChange, handle, ...rest }) => {
+  const initValue = useRef();
+  const { notUpdateSubmitNull } = options;
+  const { onFormatValue } = handle;
+
+  useWillMount(_ => {
+    initValue.current = value;
+    if (notUpdateSubmitNull) {
+      onFormatValue(name, 'toValue');
+      onChange({ _toValue: undefined });
+    }
+  });
+
+  function handleChange(e) {
+    const v = e.target.value;
+    if (notUpdateSubmitNull) {
+      onChange({
+        _toValue: v,
+      });
+    } else {
+      onChange(v);
+    }
+  }
+
+  return <Password {...props} defaultValue={initValue.current} onChange={handleChange} />;
 }
