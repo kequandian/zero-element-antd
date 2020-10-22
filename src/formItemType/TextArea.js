33f82bb517956bd239878d9862f923ded2a250e7
LLH <youyeke@163.com>
2020-10-22 10:08:25 +0800

    TextArea 现在暂时使用原生的文本域

--- src/formItemType/TextArea.js
+++ src/formItemType/TextArea.js
@@ -3,8 +3,19 @@ import { Input } from 'antd';
 
 const { TextArea } = Input;
 
-export default ({ props, ...rest }) => {
-  return <TextArea
+export default ({ props = {}, ...rest }) => {
+  const { autoSize = {} } = props;
+  const { minRows = 2 } = autoSize;
+
+  // antd 的 TextArea 用在模态框里有闪烁的问题
+  // 暂时用回原生文本域
+  return <textarea
+    className="ant-input"
+    rows={minRows}
+    {...rest}
+  />
+
+  return <Input.TextArea
     autoSize={{
       minRows: 2,
     }}
