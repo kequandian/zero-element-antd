f0bd0666747f0ea2a09bb6b575b4ea9ed3546c3c
AEdgejay <471021444@qq.com>
2021-06-29 12:14:28 +0800

    更新onRequest方法

--- src/listAction/onRequest.js
+++ src/listAction/onRequest.js
@@ -1,37 +1,113 @@
+/** 2021-6-29 新方法
+ *  将原先旧方法做了更改，添加了endpoint节点，能够通过query来进行列表API获取传值的判断
+ */
 import { query, post, update, remove, download } from 'zero-element/lib/utils/request';
 import { message as msg } from 'antd';
-
+import { get as getEndpoint } from 'zero-element/lib/utils/request/endpoint';
+const endpoint = getEndpoint()
 const methodMap = {
   'get': query,
   'post': post,
   'put': update,
   'delete': remove,
-  'download': download,
+  'download': download
 };
-
 export default function onRequest(props) {
-  const { options, record } = props;
-  const { API, method = 'get', message = '操作成功', fileNameField, data = {} } = options;
+  const {
+    options,
+    record
+  } = props;
+  const {
+    API,
+    method = 'get',
+    message = '操作成功',
+    fileNameField,
+    data = {},
+    query = {
+      id: 'id'
+    }
+  } = options;
   const match = methodMap[method];
+  Object.keys(query).forEach(toKey => {
+    const formKey = query[toKey];
+    data[toKey] = record[formKey] || formKey;
+  });
+
+  // console.log(data)
+
+  // console.log(record.rawFrameImage)
 
   if (method === 'download') {
-    return download(API, {
+    return download(endpoint+API, {
       method: options.downloadMethod,
-      fileName: record[fileNameField] || options.fileName,
-    })
-      .then(_ => {
-        if (message) {
-          msg.success(message);
-        }
-      })
-    // .catch(_ => msg.error(JSON.stringify(_)));
-  }
-
-  return match(API, data)
-    .then(_ => {
+      fileName: record[fileNameField] || options.fileName
+    }).then(_ => {
       if (message) {
         msg.success(message);
       }
-    })
-  // .catch(_ => msg.error(JSON.stringify(_)));
+    }); // .catch(_ => msg.error(JSON.stringify(_)));
+  }
+
+
+  return match(endpoint+API, data).then(_ => {
+    if (message) {
+      msg.success(message);
+    }
+  }); // .catch(_ => msg.error(JSON.stringify(_)));
 }
+
+
+
+// 旧方法
+// import { query, post, update, remove, download } from 'zero-element/lib/utils/request';
+// import { message as msg } from 'antd';
+// import { get as getEndpoint } from 'zero-element/lib/utils/request/endpoint';
+// const endpoint = getEndpoint()
+// const methodMap = {
+//   'get': query,
+//   'post': post,
+//   'put': update,
+//   'delete': remove,
+//   'download': download
+// };
+// export default function onRequest(props) {
+//   const {
+//     options,
+//     record
+//   } = props;
+//   const {
+//     API,
+//     method = 'get',
+//     message = '操作成功',
+//     fileNameField,
+//     data = {},
+//     query = {
+//       id: 'id'
+//     }
+//   } = options;
+//   const match = methodMap[method];
+//   Object.keys(query).forEach(toKey => {
+//     const formKey = query[toKey];
+//     data[toKey] = record[formKey] || formKey;
+//   });
+
+
+
+//   if (method === 'download') {
+//     return download(endpoint+API, {
+//       method: options.downloadMethod,
+//       fileName: record[fileNameField] || options.fileName
+//     }).then(_ => {
+//       if (message) {
+//         msg.success(message);
+//       }
+//     }); // .catch(_ => msg.error(JSON.stringify(_)));
+//   }
+
+
+//   return match(endpoint+API, data).then(_ => {
+//     if (message) {
+//       msg.success(message);
+//     }
+//   }); // .catch(_ => msg.error(JSON.stringify(_)));
+// }
