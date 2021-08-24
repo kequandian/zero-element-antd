02a43e5c2b34ee0547f686d8f6ef383d14c8fe76
AEdgejay <471021444@qq.com>
2021-08-24 14:50:25 +0800

    更新逻辑，添加备注

--- src/formItemType/switch.js
+++ src/formItemType/switch.js
@@ -1,3 +1,6 @@
+/**
+ * 开关组件，默认0为false，1为true
+ */
 import React from 'react';
 import { Switch } from 'antd';
 
@@ -5,12 +8,17 @@ export default function SelectWrapped(props) {
   const { value, options = [], onChange, props: p, ...rest } = props;
 
   function handleChange(value) {
+    if(value===false){
+      value=0
+    }else{
+      value=1
+    }
     onChange(value);
   }
 
   return <Switch
     onChange={handleChange}
-    value={value}
+    checked={value===1?false:true}
     {...rest}
     {...p}
   >
