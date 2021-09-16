fe3cc02c5d114d2fcfd9accaa521481ad4e1709d
AEdgejay <471021444@qq.com>
2021-09-16 17:44:04 +0800

    解决RichText为相对路径的问题

--- src/formItemType/RichText/uploadFile.js
+++ src/formItemType/RichText/uploadFile.js
@@ -1,12 +1,14 @@
 import { upload } from 'zero-element/lib/utils/request';
+import { get as getEndPoint } from 'zero-element/lib/utils/request/endpoint';
 
 export default function (api, param) {
+  const endpoint = getEndPoint()
   upload(api, { file: param.file })
     .then((response) => {
       const { data } = response.data;
       if (data) {
         param.success({
-          url: data.url,
+          url: endpoint+data.url,
         })
       }
     })
