03df1f75a2db5d47f94d7aabcfaa8740bc2743cb
AEdgejay <471021444@qq.com>
2022-01-07 02:01:43 +0800

    tag组件兼容字体颜色及字体粗细配置

--- src/valueType/tag/index.js
+++ src/valueType/tag/index.js
@@ -5,8 +5,8 @@ import '../../rewrite.less'
 
 export default function valueTypeTag(props) {
   const { options = {}, data: { text = '-' },theme,type,state} = props;
-  const { map = {},chy = {},colorMap={} } = options;
-  return <Tag className={colorMap[text]?'':`_tag_color ${theme} ${type} ${state||chy[text]}`||`_tag_color tag`} style={{"background":colorMap[text],"color":"#fff"}}>
+  const { map = {},chy = {},colorMap={},weight,fontColor } = options;
+  return <Tag className={colorMap[text]?'':`_tag_color ${theme} ${type} ${state||chy[text]}`||`_tag_color tag`} style={{"background":colorMap[text],"color":fontColor||"#fff","fontWeight":weight}}>
     {map[text] || defaultMap[text] || text}
   </Tag>
 }
