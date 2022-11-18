a22422baf22579053d07c29668796df5feb63b88
HDJ <263463764@qq.com>
2022-11-18 16:33:16 +0800

    扩展 listAction/onPath

--- src/listAction/onPath.js
+++ src/listAction/onPath.js
@@ -1,15 +1,50 @@
-import { history } from 'umi';
+// import { history } from 'umi';
 
-export default function onPath({ options, record }) {
-  const { path, query = { id: 'id' } } = options;
+// export default function onPath({ options, record }) {
+//   const { path, query = { id: 'id' } } = options;
+//   const data = {};
+//   Object.keys(query).forEach(toKey => {
+//     const formKey = query[toKey];
+//     data[toKey] = record[formKey] || formKey;
+//   });
+
+//   history.push({
+//     pathname: path,
+//     query: data,
+//   });
+// }
+
+// new code
+import React from 'react';
+import { history } from 'umi';
+import { Button } from 'antd';
+import qs from 'qs';
+
+function handleClick(path, data, blank) {
+  if (blank) {
+    const url = qs.stringify(data);
+    window.open(`${path}?${url}`)
+  } else {
+    history.push({
+      pathname: path,
+      query: data,
+    });
+  }
+}
+export default (props) => {
+  const { data: { text = '', record }, options = {} } = props;
+  const { path, query = { id: 'id' }, blank = false } = options;
   const data = {};
+
   Object.keys(query).forEach(toKey => {
     const formKey = query[toKey];
     data[toKey] = record[formKey] || formKey;
   });
 
-  history.push({
-    pathname: path,
-    query: data,
-  });
+  if (text) {
+    return <Button type="link" onClick={handleClick.bind(null, path, data, blank)}>
+      {text}
+    </Button>;
+  }
+  return null;
 }
